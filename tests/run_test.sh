c44e9c531bf1fe8b00579097f1f19fe90c11c22e
Matthew Fleeger <mfleeger5@gmail.com>
2016-12-27 15:50:31 -0500

    Removed bundle install

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gem install bundler && bundle install && cd tests && xvfb-run -s "-screen 0 1024x768x16" rspec gimage_rtest.rb
+xvfb-run -s "-screen 0 1024x768x16" rspec gimage_rtest.rb
