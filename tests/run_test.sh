1a526afb0b97bd92982961a54d0fa722c4b99a75
Matthew Fleeger <mfleeger5@gmail.com>
2016-12-27 15:48:43 -0500

    Removed /vagrant references.

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /vagrant && gem install bundler && bundle install && cd /vagrant/tests && xvfb-run -s "-screen 0 1024x768x16" rspec gimage_rtest.rb
+gem install bundler && bundle install && cd tests && xvfb-run -s "-screen 0 1024x768x16" rspec gimage_rtest.rb
